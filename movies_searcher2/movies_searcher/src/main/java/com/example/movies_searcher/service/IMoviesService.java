5dccdf850fd1f3298754dead3d6301431ec28378
e-sdponce <e-sdponce@neoris.com>
2025-01-13 12:38:30 -0600

    Se elimino los comentarios y código que no se utiliza

--- movies_searcher2/movies_searcher/src/main/java/com/example/movies_searcher/service/IMoviesService.java
+++ movies_searcher2/movies_searcher/src/main/java/com/example/movies_searcher/service/IMoviesService.java
@@ -5,21 +5,19 @@ import com.example.movies_searcher.model.Movies;
 import java.util.List;
 import java.util.Optional;
 
+// Interfaz de Servicio
+
 public interface IMoviesService {
     public List<Movies> listarPeliculas();
 
     public Movies buscarporId(Integer idMovie);
-    //public List<Movies> findByTitle(String film);
 
     public List<Movies> buscarporNombre(String nombre);
 
-
     Optional<Movies> obtenerPeliculaAleatoria();
 
-   // public List<Movies> buscarProveedor(String leadStudio);
 
    public Movies saveorUpdateMovie(Movies movies);
-
 
     List<Movies> filtrarporStudio(String leadStudio);
 
